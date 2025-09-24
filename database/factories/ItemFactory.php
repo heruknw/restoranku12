1d21ddbdad1369d17bdddfb6c7d8eb3c7f07b7ca
Heru kurniawan <heru270500@gmail.com>
2025-09-24 09:07:12 +0700

    Add new assets and SCSS helpers, mixins, and utilities for improved styling

--- database/factories/ItemFactory.php
+++ database/factories/ItemFactory.php
@@ -18,10 +18,16 @@ class ItemFactory extends Factory
     {
         return [
             'name' => $this->faker->name(),
-            'category_id' =>$this->faker->number_Between(1, 2),
+            'category_id' =>$this->faker->numberBetween(1, 2),
             'price' =>$this->faker->randomFloat(2, 1000, 100000),
             'description' =>$this->faker->text(),
-            'img' =>$this->faker->imageUrl(),
+            'img' =>fake()->randomElement(
+                [
+                    'https://media.istockphoto.com/id/1302143931/id/foto/sup-ramen-di-atas-meja-kayu-pedesaan.jpg?s=2048x2048&w=is&k=20&c=WK0a1dVEFfhbF898DccQGpTZPXmoWg0ymXeP5jjrlEg=',
+                    'https://plus.unsplash.com/premium_photo-1668146927669-f2edf6e86f6f?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+                    'https://media.istockphoto.com/id/1555947107/id/foto/set-sushi-dan-maki.jpg?s=2048x2048&w=is&k=20&c=NfruJqcbV7UTBjha2VseuzfN3Vh8d6gO8G3TLzXkFds=',
+                ]
+            ),
             'is_active' =>$this->faker->boolean(),
         ];
     }
--- database/migrations/2025_09_19_120605_create_items_table.php
+++ database/migrations/2025_09_19_120605_create_items_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->integer('price');
             $table->unsignedBigInteger('category_id');
             $table->string('img')->nullable();
-            $table->boolean('is_active')->dafault(true);
+            $table->boolean('is_active')->default(true);
             $table->softDeletes();
             $table->timestamps();
 
